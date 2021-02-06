cee3a1d2b65b4cecc24f9321c5b5d35532d460ed
saragolkaran <64063997+saragolkaran@users.noreply.github.com>
2021-02-06 19:31:18 +0800

    Update config.js

--- experimentsecuritywithcompetitionsystem/src/config/config.js
+++ experimentsecuritywithcompetitionsystem/src/config/config.js
@@ -14,3 +14,14 @@ module.exports = {
 };
 //Reference:
 //https://medium.com/the-node-js-collection/making-your-node-js-work-everywhere-with-environment-variables-2da8cdf6e786
+
+// window._config = {
+//     cognito: {
+//         userPoolId: ' us-east-1_hAllWSGVl', // e.g. us-east-2_uXboG5pAb
+//         userPoolClientId: '52lk6cm19dqtejddh94thc6o0l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+//         region: 'us-east-1' // e.g. us-east-2
+//     },
+//     api: {
+//         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+//     }
+// };
